de0251b3fcd237557fb6edab1169cdea2288f0a8
Zjotjawik <zjotjawik@gmail.com>
2023-09-18 11:43:14 +0200

    cloudinary support implemented

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const cloudinary = require('cloudinary');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const connectDB = require('./utils/connectDB');
 const treatmentRoutes = require('./routes/treatmentRoutes');
 const userRoutes = require('./routes/userRoutes'); 
+
+          
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
