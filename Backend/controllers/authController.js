16fcfee07c7492781c7bb4d2f2d1b980963a0ce5
Zjotjawik <zjotjawik@gmail.com>
2023-09-19 12:16:43 +0200

    added basic auth process

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,69 @@
+const User = require('../schemas/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// ... (existing code)
+
+const signUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    // Generate JWT token
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h', // Token expiration time (adjust as needed)
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Error creating user' });
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h', // Token expiration time (adjust as needed)
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error('Error signing in:', error);
+    res.status(500).json({ error: 'Error signing in' });
+  }
+};
+
+module.exports = { signUp, signIn };
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const authMiddleware = require('../utils/authMiddleware');
+
+// Protected route (example)
+router.get('/profile', authMiddleware.authenticateToken, (req, res) => {
+  // Access authenticated user via req.user
+  res.json({ message: 'Accessed protected route', user: req.user });
+});
 
 router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+router.post('/signup', authController.signUp);
+router.post('/signin', authController.signIn);
 
 module.exports = router;
