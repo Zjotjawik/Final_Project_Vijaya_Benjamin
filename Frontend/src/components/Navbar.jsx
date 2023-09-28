cd8f025eabe4cf90c58a70b8f3c56c5e0f2fdab5
vijayavalluri <100075809+vijayavalluri@users.noreply.github.com>
2023-09-28 10:13:24 +0200

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,33 +5,6 @@ import SearchBar from "../components/SearchBar.jsx";
 import SearchResultsList from "../components/SearchResultsList.jsx";
 // import { Dropdown } from 'bootstrap';
 import DropdownAilments from '../components/DropdownAilments';
-  return (
-    <>
-    <nav >
-      {/* <Link to="/" className="logo"><img src="https://pranadaayurved.in/wp-content/uploads/2022/09/Pranada-Logo-tree.png" alt="logo" /></Link> */}
-      <Link to="/" className="title"><img className="logo" src="https://pranadaayurved.in/wp-content/uploads/2022/09/Pranada-Logo-tree.png" alt="logo" />AyurEase<span>Your Guide to Natural Healing</span></Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-      <span></span>
-      <span></span>
-      <span></span>
-      </div>
-      <ul className={menuOpen ? "open" : ""}>
-          <li><NavLink to="/" >Home</NavLink></li>
-          <li><NavLink to="/about">About us</NavLink></li>
-          <li onMouseEnter={()=> setOpenAilments(true)} onMouseLeave={()=> setOpenAilments(false)}><NavLink to="/ailments" >Ailments</NavLink>
-              {openAilments && <DropdownAilments />}
-          </li>
-          <li><NavLink to="/ingredients">Ayur Ingredients</NavLink></li>
-          <li><NavLink to="/suggestion-form">Suggestion form</NavLink></li>
-          <li><NavLink to="/auth/signin">Sign in</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign up</NavLink></li>
-      </ul>
-      <div className="search-results-container">
-      <SearchBar setResults={setResults} setCriteria={setCriteria}/> 
-      {results.length > 0 && <SearchResultsList results={results} />}
-      </div>
-    </nav>
-=======
 import Axios from 'axios';
 
 
