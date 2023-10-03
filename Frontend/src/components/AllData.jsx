2552323dc7f17e89b79552c78feffc67f4ed7521
Zjotjawik <zjotjawik@gmail.com>
2023-10-03 18:22:51 +0200

    Modified AllData.jsx to filter out ailment Database entries

--- Frontend/src/components/AllData.jsx
+++ Frontend/src/components/AllData.jsx
@@ -1,38 +1,56 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import '../styles/AllData.css';
 
-const AllData = ({data, setData}) => {
+const AllData = ({ data, setData }) => {
+  // Filter out entries that contain the key "ailment"
+  const filteredData = data.filter((item) => !item.hasOwnProperty('ailment'));
+
   return (
     <>
-      { data.length > 0 && data.map((item) => { return (
-        // <div className='container '>
-        <div key={item._id} className='container-innertext'> 
-            <div key={item._id} className='row-alldata '>
-            <div className='col-md-4 '> 
-                <img className='img-responsive rounded-end  ' src={item.picture} alt="Plant" />
-              </div>
-              <div className='col-md-8 '>
-                 
-                    <h2 className='hindi-names text-center font-bold'>
-                    <span className='span-hindi'>Hindi Names:</span>{''} {item.hindiName.join(', ')}</h2>
-                     <h2 className='english-names text-center font-bold'> <span className='span-english '>English Names:</span>{''} {item.englishName.join(', ')}</h2> 
+      {filteredData.length > 0 &&
+        filteredData.map((item) => {
+          return (
+            <div key={item._id} className='container-innertext'>
+              <div key={item._id} className='row-alldata'>
+                <div className='col-md-4 '>
+                  <img
+                    className='img-responsive rounded-end '
+                    src={item.picture}
+                    alt='Plant'
+                  />
+                </div>
+                <div className='col-md-8'>
+                  <h2 className='hindi-names text-center font-bold'>
+                    <span className='span-hindi'>Hindi Names:</span> {''}
+                    {item.hindiName.join(', ')}
+                  </h2>
+                  <h2 className='english-names text-center font-bold'>
+                    {' '}
+                    <span className='span-english '>English Names:</span> {''}
+                    {item.englishName.join(', ')}
+                  </h2>
                   <ul className='text-left'>
-                  <h3 className='medical-uses font-bold'>Medical Uses:</h3>
+                    <h3 className='medical-uses font-bold'>Medical Uses:</h3>
                     {item.medicalUses.map((use, index) => (
-                      <li key={index} >
-                      <p className='symptom-name'><strong className='symptom'>Symptom:</strong> {use.symptom} </p>  
-                        <p className='description-all'> <strong>
-                        Description:</strong> {use.description}</p>
+                      <li key={index}>
+                        <p className='symptom-name'>
+                          <strong className='symptom'>Symptom:</strong>{' '}
+                          {use.symptom}{' '}
+                        </p>
+                        <p className='description-all'>
+                          {' '}
+                          <strong>Description:</strong> {use.description}
+                        </p>
                       </li>
                     ))}
                   </ul>
+                </div>
               </div>
-             
             </div>
-        </div>
-        // </div>
-    )})}
+          );
+        })}
     </>
   );
-}
+};
+
 export default AllData;
